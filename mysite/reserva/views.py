bdf92ae7cb2c36413f1d4004421bcf77b02bc8a0
JuliaRecalde <yuliantonia@gmail.com>
2023-04-28 18:08:05 -0400

    listado de reservas: sin terminar

--- mysite/reserva/views.py
+++ mysite/reserva/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from django.http import Http404
+
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
+
+
+from django.shortcuts import render
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "reserva/index.html", context)
+
+
